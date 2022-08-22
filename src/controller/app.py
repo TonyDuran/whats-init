105b2767bca3a3f00e661654e81051480f6d2cb1
Tony Duran <tony-duran@outlook.com>
2022-08-22 02:16:37 +0000

    demo communication between worker/controller

---
+++ src/controller/app.py
@@ -0,0 +1,30 @@
+import asyncio
+import multiprocessing
+
+async def send_message(message):
+    reader, writer = await asyncio.open_connection(
+        '127.0.0.1', 8888)
+
+    print(f'Send: {message!r}')
+    writer.write(message.encode())
+
+    data = await reader.read(100)
+    print(f'Received: {data.decode()!r}')
+
+    print('Close the connection')
+    writer.close()
+
+def target(message):
+    asyncio.run(send_message(message))
+
+def main():
+    # terrible example to test spamming server
+    urls = ['google.com',
+            'bing.com',
+            'example.com'] * 100
+
+    with multiprocessing.Pool(3) as p:
+        p.map(target, urls)
+
+if __name__ == '__main__':
+    main()
--- src/worker/app.py
+++ src/worker/app.py
@@ -1,9 +1,27 @@
 import pathlib
+import asyncio
 
-def main():
-    print('stub')
+async def handle_message(reader, writer):
+    data = await reader.read(100)
+    message = data.decode()
+    addr = writer.get_extra_info('peername')
+
+    print(f"Received message from {message!r} from {addr!r}")
+    print(f"Sending: {message!r}")
+    writer.write(data)
+    await writer.drain()
+    print("Closing connection")
+    writer.close()
+
+async def main():
+    server = await asyncio.start_server(handle_message, 'localhost', 8888)
+    addrs = ', '.join(str(sock.getsockname()) for sock in server.sockets)
+    print(f'Serving on {addrs}')
+
+    async with server:
+        await server.serve_forever()
 
 
 
 if __name__ == '__main__':
-    main()
+    asyncio.run(main())
