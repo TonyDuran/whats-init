592914456c763b9fec18b256da06471a4e9bfc4c
Tony Duran <tony-duran@outlook.com>
2022-10-08 20:26:27 -0500

    linted py files

--- src/controller/app.py
+++ src/controller/app.py
@@ -1,31 +1,37 @@
 #!/usr/bin/env python3
 import asyncio
 import multiprocessing
+from heimdall import Message
+
 
 async def send_message(message):
-    reader, writer = await asyncio.open_connection(
-        '127.0.0.1', 8888)
+    reader, writer = await asyncio.open_connection("127.0.0.1", 8888)
 
-    print(f'Send: {message!r}')
+    print(f"Send: {message!r}")
     writer.write(message.encode())
 
     data = await reader.read(100)
-    print(f'Received: {data.decode()!r}')
+    print(f"Received: {data.decode()!r}")
 
-    print('Close the connection')
+    print("Close the connection")
     writer.close()
+
 
 def target(message):
     asyncio.run(send_message(message))
 
+
+def register_worker():
+    pass
+
+
 def main():
     # terrible example to test spamming server
-    urls = ['google.com',
-            'bing.com',
-            'example.com'] * 100
+    urls = ["google.com", "bing.com", "example.com"] * 100
 
     with multiprocessing.Pool(3) as p:
         p.map(target, urls)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
